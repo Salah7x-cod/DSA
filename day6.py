a69d0b48b52f6cc2d6f3ca30b1d7b8234ba44951
Salah7x-cod <salahudin.al.ayubii63@gmail.com>
2026-07-16 11:47:51 +0300

    Fix formatting of comments in day6.py to improve readability

--- day6.py
+++ day6.py
@@ -1,37 +1,37 @@
 if __name__ == '__main__':
     N = int(input())
-START
-Initialize an empty list: my_list = []
+#START
+#Initialize an empty list: my_list = []
 
-Read integer n  (number of commands)
+#Read integer n  (number of commands)
 
-FOR each of the n lines:
-    Read the command line as a string
-    Split the string into parts (words)
+#FOR each of the n lines:
+    #Read the command line as a string
+    #Split the string into parts (words)
 
-    IF first word == "insert":
-        position = second word (convert to int)
-        value = third word (convert to int)
-        insert value at position in my_list
+    #IF first word == "insert":
+        #position = second word (convert to int)
+        #value = third word (convert to int)
+        #insert value at position in my_list
 
-    ELSE IF first word == "print":
-        print my_list
+    #ELSE IF first word == "print":
+        #print my_list
 
-    ELSE IF first word == "remove":
-        value = second word (convert to int)
-        remove first occurrence of value from my_list
+    #ELSE IF first word == "remove":
+        #value = second word (convert to int)
+        #remove first occurrence of value from my_list
 
-    ELSE IF first word == "append":
-        value = second word (convert to int)
-        append value to my_list
+    #ELSE IF first word == "append":
+        #value = second word (convert to int)
+        #append value to my_list
 
-    ELSE IF first word == "sort":
-        sort my_list
+    #ELSE IF first word == "sort":
+        #sort my_list
 
-    ELSE IF first word == "pop":
-        remove last element from my_list
+    #ELSE IF first word == "pop":
+        #remove last element from my_list
 
-    ELSE IF first word == "reverse":
-        reverse my_list
+    #ELSE IF first word == "reverse":
+        #reverse my_list
 
-END FOR
+#END FOR
